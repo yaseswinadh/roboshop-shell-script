06920c63f668119cdfb1c25d842b0f341055127f
yaseswi <yaseswi111@gmail.com>
2022-08-17 16:52:17 +0530

    second commit

--- Components/Roboshop/Frontend.sh
+++ Components/Roboshop/Frontend.sh
@@ -7,5 +7,7 @@ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/fron
 cd /usr/share/nginx/html
 rm -rf *
 unzip /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
