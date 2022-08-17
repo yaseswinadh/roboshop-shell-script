ca824fd84da7d64fe8778f91dd4aaa2e128a259f
yaseswi <yaseswi111@gmail.com>
2022-08-17 16:30:28 +0530

    read command

--- Components/Roboshop/Frontend.sh
+++ Components/Roboshop/Frontend.sh
@@ -1,1 +1,11 @@
-echo install Frontend compoent
+#!bin/bash
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
