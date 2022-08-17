7ace4f76f8ae3920a3756efab1dc068494a6901c
yaseswi <yaseswi111@gmail.com>
2022-08-17 19:07:22 +0530

    Fourth commit

--- Components/Roboshop/mongoDB.sh
+++ Components/Roboshop/mongoDB.sh
@@ -5,7 +5,7 @@ systemctl start mongod
 
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongdb
+systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
