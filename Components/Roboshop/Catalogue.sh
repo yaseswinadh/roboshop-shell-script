9ef0fd5bf57f126669820c124c05e960c3858f39
yaseswi <yaseswi111@gmail.com>
2022-08-17 19:20:19 +0530

    first commit

--- Components/Roboshop/Catalogue.sh
+++ Components/Roboshop/Catalogue.sh
@@ -1,1 +1,23 @@
-echo install catalogue component
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+useradd roboshop
+
+
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/catalogue.zip
+mv catalogue-main catalogue
+cd /home/roboshop/catalogue
+npm install
+
+#1. Update SystemD file with correct IP addresses
+#
+# Update  'MONGO DSNAME' with MongDB server IP
+#
+#2. Now, lets set up the service with systemctl.
+
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl start catalogue
+systemctl enable catalogue
